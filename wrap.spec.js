b0d34064daa4b4f3fbb54236a11e22ad09980e7f
Tom <tomsinovich@gmail.com>
2019-06-27 10:49:30 -0500

    wrap.spec.js solution

--- wrap.spec.js
+++ wrap.spec.js
@@ -2,6 +2,17 @@ const { expect } = require("chai");
 const wrap = require("./wrap");
 
 describe("wrap", () => {
+
+  let what = false;
+
+  beforeEach(() => {
+    wrap("this is new line you guys i swear", 5).split('\n').forEach(x => {
+      if (x.length <= 5) {
+        what = true
+      }
+    })
+  })
+
   it("Returns empty string if empty string was provided", () => {
     expect(wrap("", 10)).to.equal("");
   });
@@ -13,4 +24,10 @@ describe("wrap", () => {
       true
     );
   });
+  it("Every line is less than or equal to column", () => {
+    expect(what)
+      .to.equal(
+      true
+    )
+  })
 });
